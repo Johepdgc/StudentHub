8688011561f8897b97f6305b8e4a10a9f3caa49a
Johepdgc <johepdg07@icloud.com>
2023-11-22 10:01:31 -0600

    Styling to login and register

---
+++ Home.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+export default function CreatePost() {
+  const [postContent, setPostContent] = useState('');
+
+  const handleSubmit = () => {
+    // Add the logic to handle post submission
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        multiline
+        numberOfLines={4}
+        placeholder="What's on your mind?"
+        value={postContent}
+        onChangeText={setPostContent}
+      />
+      <Button title="Post" onPress={handleSubmit} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  input: {
+    height: 80,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingLeft: 8,
+  },
+});
--- Login.js
+++ Login.js
@@ -1,22 +1,34 @@
 import React from 'react';
-import { View, TextInput, Pressable, StyleSheet } from 'react-native';
+import { View, TextInput, Pressable, StyleSheet, Text, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
 
 const Login = () => {
+    const navigator = useNavigation();
     return (
         <View style={styles.container}>
-            <TextInput
-                style={styles.input}
-                placeholder="Usuario"
-                // logica para controlar el input de usuario
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Contraseña"
-                // logica para controlar el input de contraseña
-            />
-            <Pressable style={styles.button} on onPress={() => {/* logica para validar el usuario y contraseña */ }}>
-                <Text style={styles.buttonText}>Ingresar</Text>
-            </Pressable>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.logo}
+                    source={require('./assets/logo.png')}
+                />
+            </View>
+            <View style={styles.loginContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Usuario"
+                    // logica para controlar el input de usuario
+                
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Contraseña"
+                    // logica para controlar el input de contraseña
+                />
+                <Pressable style={styles.button} on onPress={() => (navigator.navigate('CreatePost'))}>
+                    <Text style={styles.buttonText}>Ingresar</Text>
+                </Pressable>
+            </View>
         </View>
     );
 };
@@ -24,15 +36,35 @@ const Login = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
+        backgroundColor: '#0D203C',
+        alignItems: 'center',
+        justifyContent: 'center',
         padding: 20,
     },
+    imageContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        width: 158,
+        height: 134,
+        marginBottom: 20,
+        marginTop: 20,
+    },
+    loginContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     input: {
-        height: 40,
-        margin: 12,
+        height: 50,
+        width: 250,
         borderWidth: 1,
         padding: 10,
-        borderRadius: 10,
+        borderRadius: 30,
+        backgroundColor: 'white',
+        textAlign: 'center',
+        marginBottom: 10,
+        marginTop: 10,
     },
     button: {
         alignItems: 'center',
@@ -42,16 +74,17 @@ const styles = StyleSheet.create({
         borderRadius: 30,
         elevation: 3,
         backgroundColor: '#F2C94C',
-        width: 200,
+        width: 150,
         height: 50,
         marginBottom: 20,
+        marginTop: 20,
     },
     buttonText: {
         fontSize: 16,
         lineHeight: 21,
         fontWeight: 'bold',
         letterSpacing: 0.25,
-        color: '#000000',
+        color: 'white',
     },
 });
 
