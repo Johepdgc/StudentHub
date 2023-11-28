98604fdc6f1bbd584f47b0c2d23bbb50831780ad
AudeliClaros <claros123naser@gmail.com>
2023-11-27 19:17:50 -0600

    proyecto Actualizado

---
+++ Groups.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Groups() {
+    return (
+        <View>
+            {/* logica para mostrar los grupos */}
+        </View>
+    );
+}
